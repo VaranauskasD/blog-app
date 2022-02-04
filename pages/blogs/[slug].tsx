6db9ab06c5f3a25942d90f631519d44d0ab89bb3
Varnas <dvaranauskas7@gmail.com>
2022-02-04 22:50:41 +0000

    added: wip dynamic routes

---
+++ pages/blogs/[slug].tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Blog: NextPage = () => {
+  return <div>Blog</div>
+}
+
+export default Blog
---
+++ pages/blogs/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Blogs: NextPage = () => {
+  return <div>Blogs</div>
+}
+
+export default Blogs
