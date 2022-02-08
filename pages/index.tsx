ceea8c08570855dbc7db4f8b22907d4e5a28fffd
varanauskasd <dvaranauskas7@gmail.com>
2022-02-08 21:20:52 +0200

    added: wip blogs

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,13 +22,47 @@ const BlogsContainer = styled.div`
   font-size: 12px;
 `
 
-const BlogContainer = styled.a``
+const BlogContainer = styled.a`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  min-height: 96px;
+  font-size: 14px;
+  padding: 8px;
+  margin: 8px 0;
+  border: 1.5px solid #777;
+  border-radius: 8px;
+
+  > span {
+    font-size: 16px;
+    text-decoration: underline;
+  }
+`
+
+const BlogInfo = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
 
 const HeaderSearch = styled.form``
 
 const blogs = [
   {
     _id: '61fe38d6fbf8f41ed4a90438',
+    title: 'Test Blog',
+    author: 'Test User',
+    body: 'This is a test blog.',
+    date: '1644050612750',
+  },
+  {
+    _id: '61fe38d6fbf8f41ed4a90491',
+    title: 'Test Blog',
+    author: 'Test User',
+    body: 'This is a test blog.',
+    date: '1644050612750',
+  },
+  {
+    _id: '61fe38d6fbf8f41ed4a90444',
     title: 'Test Blog',
     author: 'Test User',
     body: 'This is a test blog.',
@@ -42,10 +76,17 @@ const Home: NextPage = () => {
       <Header />
       <ContentContainer>
         <BlogsContainer>
+          <h1>Blogs</h1>
           {blogs.map((blog) => (
-            <BlogContainer>
-              {blog.title} {blog.author}
-            </BlogContainer>
+            <Link href={blog._id} passHref={true}>
+              <BlogContainer>
+                <span>{blog.title}</span>
+                <BlogInfo>
+                  <span>{blog.author}</span>
+                  <span>{blog.date}</span>
+                </BlogInfo>
+              </BlogContainer>
+            </Link>
           ))}
         </BlogsContainer>
       </ContentContainer>
